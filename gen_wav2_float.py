828fb7a8857b09ce22e65577d3dd5e08ba154ffa
Jens Peter Nielsen <jpn9900@gmail.com>
2023-01-24 05:27:37 +0100

    Create gen_wav2_float.py
    A faster version of gen_wave_float.py
    
    assisted by github CoPilot and the instructions:
    
    """optimize the code by using numpy array and vectorization of the code."""

---
+++ gen_wav2_float.py
@@ -0,0 +1,28 @@
+import numpy as np
+import scipy.io.wavfile as scipy_io_wavfile
+from scipy import signal
+
+# Settings
+sampleRate = 44100  # of samples per second (standard)
+numSamples = 2048  # of samples per waveform
+numWaves = 128  # of waveforms in wavetable
+pi = np.pi  # the constant pi
+
+# Define wavetable as function of time 0 <= t < 1
+# and interpolation parameter 0 <= x < 1
+
+def waves(t, x):
+    return signal.sawtooth(2 * pi * t, x)
+
+# Loop through waveforms in wavetable
+x = np.linspace(0, 1, numWaves)
+t = np.linspace(0, 1, numSamples)
+
+# Loop through samples in waveform
+data = np.empty(0, dtype=np.float32)  # empty float32 array
+for waveIndex in range(numWaves):
+    data = np.append(data, waves(t, x[waveIndex]))
+
+# Generate .wav file
+
+scipy_io_wavfile.write("wavetable.wav", sampleRate, data.astype('float32'))
